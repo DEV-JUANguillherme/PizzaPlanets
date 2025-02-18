c15df399bfebf7ef8ec737c6421bc0a6452212fe
Juan Guillherme <guilhermejuan729@gmail.com>
2025-02-17 23:51:26 -0300

    feat: testing

---
+++ src/routes/router.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+//import { CreateUserController } from './controllers/user/CreateUserController';
+//import { AuthLoginController } from './controllers/user/AuthLoginController';
+
+const router = Router();
+
+//router.post('/users', new CreateUserController().handle)
+//router.post('/session', new AuthLoginController().handle)
+
+export { router }; 
